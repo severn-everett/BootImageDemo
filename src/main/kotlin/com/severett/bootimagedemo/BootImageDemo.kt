2a83cf2ae76658fa159e740ab0834be52dcf1c37
Severn Everett <severn.everett@gmail.com>
2022-12-05 08:48:41 +0100

    Fixing root application class to better conform to Kotlin style

--- src/main/kotlin/com/severett/bootimagedemo/BootImageDemo.kt
+++ src/main/kotlin/com/severett/bootimagedemo/BootImageDemo.kt
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class BootImageDemo {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(BootImageDemo::class.java, *args)
-        }
-    }
+class BootImageDemo
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BootImageDemo::class.java, *args)
 }
