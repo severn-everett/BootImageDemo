2a0383fd639b8b8310e24bc19916ff08e1b3e102
Severn Everett <severn.everett@gmail.com>
2022-12-29 13:45:12 -0800

    Upgraded Kotlin to 1.8.0; Upgraded Spring Boot to 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_19
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.allopen") version "1.7.21"
-    kotlin("plugin.jpa") version "1.7.21"
-    kotlin("plugin.serialization") version "1.7.21"
-    id("org.springframework.boot") version "3.0.0"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.allopen") version "1.8.0"
+    kotlin("plugin.jpa") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
+    id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
@@ -25,7 +26,9 @@ apply(plugin = "io.spring.dependency-management")
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(JVM_19)
+        }
     }
     test {
         useJUnitPlatform()
@@ -36,7 +39,7 @@ dependencies {
     val kotlinCoroutinesVersion: String by project
     //// Production Dependencies
     // Implementation
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     implementation("io.github.microutils:kotlin-logging:3.0.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
