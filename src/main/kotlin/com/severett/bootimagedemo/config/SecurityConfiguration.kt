bc6cbbcf14b07df6e64d9c1f676e0beeb09c809e
Severn Everett <severn.everett@gmail.com>
2023-01-13 10:27:56 +0100

    04_configurable_loading Converted profile-based security loading to code-based security loading

--- src/main/kotlin/com/severett/bootimagedemo/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/severett/bootimagedemo/config/SecurityConfiguration.kt
@@ -2,21 +2,31 @@ package com.severett.bootimagedemo.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 
+private const val SECURITY_ENABLED = "bootimagedemo.securityEnabled"
 
 @Configuration
-@Profile("withSecurity")
-class SecurityConfiguration {
+class SecurityConfiguration(private val env: Environment) {
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http.authorizeExchange { exchanges ->
+        val securityEnabled = env[SECURITY_ENABLED]?.toBoolean() ?: false
+        return if (securityEnabled) {
+            http.authorizeExchange { exchanges ->
                 exchanges.anyExchange().authenticated()
             }
-            .httpBasic(withDefaults())
-            .build()
+                .httpBasic(withDefaults())
+                .build()
+        } else {
+            http.authorizeExchange { exchanges ->
+                exchanges.anyExchange().permitAll()
+            }
+                .csrf().disable()
+                .build()
+        }
     }
 }
