dab1632846c09d8fbf8329fc774afa074e92e0a6
Severn Everett <severn.everett@gmail.com>
2023-03-24 14:11:44 +0100

    Upgraded Spring Boot to 3.0.5; Upgraded Kotlin Logging to 3.0.5; Upgraded Kotlin Serialization to 1.5; Upgraded Gradle to 8.0.2

--- src/main/kotlin/com/severett/bootimagedemo/controller/ControllerAdvice.kt
+++ src/main/kotlin/com/severett/bootimagedemo/controller/ControllerAdvice.kt
@@ -6,12 +6,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler
-import reactor.core.publisher.Mono
 
 @ControllerAdvice
 class ControllerAdvice : ResponseEntityExceptionHandler() {
     @ExceptionHandler(EntityNotFoundException::class)
-    fun handleEntityNotFoundException(): Mono<ResponseEntity<Unit>> {
-        return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).build())
+    fun handleEntityNotFoundException(): ResponseEntity<Unit> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build()
     }
 }
